74735e56cad9389d4f0fa8e1425134a823f7ea95
nickparedes <nick.paredes@unmsm.edu.pe>
2023-02-21 02:16:33 -0500

    definiendo estructura del proyecto

---
+++ src/main/java/com/todo/todoapi/controller/TaskController.java
@@ -0,0 +1,11 @@
+package com.todo.todoapi.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+    public void hola (){
+        System.out.println("hola como estas");
+    }
+}
